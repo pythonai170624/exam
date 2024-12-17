ca99eee94e3e1a4676ed74c85f8101a80a086b59
pythonai170624 <pythonai170624@gmail.com>
2024-12-17 09:37:56 +0200

    Create seker.py

---
+++ seker.py
@@ -0,0 +1,7 @@
+data = [
+    {"cust_id": 1, "timestamp": "2024-01-01", "residence": "Tel Aviv", "grade": 8},
+    {"cust_id": 2, "timestamp": "2024-01-02", "residence": "Jerusalem", "grade": None},
+    {"cust_id": 1, "timestamp": "2024-01-03", "residence": "Tel Aviv", "grade": 8},
+    {"cust_id": 3, "timestamp": "2024-01-04", "residence": None, "grade": 7},
+    {"cust_id": 2, "timestamp": "2024-01-05", "residence": "Jerusalem", "grade": None}
+]
